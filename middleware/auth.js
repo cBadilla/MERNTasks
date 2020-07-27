b7602ad5690222cfd60fdaeaf613ec7bfdde439f
cBadilla <badilla885@gmail.com>
2020-07-27 08:37:37 -0600

    ajustes para deployment

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,16 +8,6 @@ module.exports = function(req, res, next){
      // Website you wish to allow to connect
      res.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
 
-     // Request methods you wish to allow
-     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
- 
-     // Request headers you wish to allow
-     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
- 
-     // Set to true if you need the website to include cookies in the requests sent
-     // to the API (e.g. in case you use sessions)
-     res.setHeader('Access-Control-Allow-Credentials', true);
-
     //Revisar el toquen
     if (!token) {
         return res.status(401).json({msg: 'No hay token, permiso no válido'})
