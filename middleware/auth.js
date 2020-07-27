d0c51511d2823793623f0821416f9c88803aeca6
cBadilla <badilla885@gmail.com>
2020-07-26 21:55:40 -0600

    ajuste para deployment

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,8 +5,18 @@ module.exports = function(req, res, next){
     //Leer el toquen del header
     const token = req.header('x-auth-token');
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+     // Website you wish to allow to connect
+     res.setHeader('Access-Control-Allow-Origin', '*');
+
+     // Request methods you wish to allow
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+     // Request headers you wish to allow
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+     // Set to true if you need the website to include cookies in the requests sent
+     // to the API (e.g. in case you use sessions)
+     res.setHeader('Access-Control-Allow-Credentials', true);
 
     //Revisar el toquen
     if (!token) {
